4c6f5798ba691e695794b4ddcc50b3254c75847f
Andre Chu <andrechu@Andres-MacBook-Air.local>
2023-05-17 14:45:29 -0700

    Updated analysis.py

--- analysis.py
+++ analysis.py
@@ -2,98 +2,79 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 import statsmodels.stats.multitest as smm
-import argparse
 from sklearn.preprocessing import scale
 from sklearn.decomposition import PCA
 
-class DifferentialExpression:
-    def __init__(self, data_file, metadata_file):
-        self.df = self.load_data(data_file)
-        self.metadata = self.load_metadata(metadata_file)
-        self.check_data_quality()
+def load_data(file_path):
+    df = pd.read_csv(file_path, sep='\t', index_col=0)
+    if df.isnull().sum().sum() > 0:
+        raise ValueError("Input data contains missing values.")
+    if df.nunique(axis=1).min() == 1:
+        raise ValueError("Input data contains rows with constant values.")
+    return df
 
-    def load_data(self, data_file):
-        df = pd.read_csv(data_file, sep='\t', index_col=0)
-        return df
+def define_metadata(df):
+    metadata = pd.DataFrame({
+        'condition': ["WT"] * 3 + ["Adar1KI"] * 3 + ["Adar1_p110KI"] * 3 + ["Adar1_p150KI"] * 3 + ["E861AKI"] * 3
+    }, index=df.columns)
+    return metadata
 
-    def load_metadata(self, metadata_file):
-        metadata = pd.read_csv(metadata_file, sep='\t', index_col=0)
-        return metadata
+def normalize_counts(df, pseudocount=1):
+    df = df.replace(0, pseudocount)
+    gm = df.prod(axis=1) ** (1.0 / len(df.columns))
+    df = df.div(gm, axis=0)
+    size_factors = df.median(axis=0)
+    df = df.div(size_factors, axis=1)
+    return df
 
-    def check_data_quality(self):
-        if self.df.isnull().sum().sum() > 0:
-            raise ValueError("Input data contains missing values.")
-        if self.df.nunique(axis=1).min() == 1:
-            raise ValueError("Input data contains rows with constant values.")
+def batch_correction(df, n_components=2):
+    pca = PCA(n_components=n_components)
+    pca_result = pca.fit_transform(scale(df.T))
+    corrected = df - pca_result.T.dot(pca.components_)
+    return corrected
 
-    def normalize_counts(self, df, pseudocount=1):
-        df = df.replace(0, pseudocount)
-        gm = df.prod(axis=1) ** (1.0 / len(df.columns))
-        df = df.div(gm, axis=0)
-        size_factors = df.median(axis=0)
-        df = df.div(size_factors, axis=1)
-        return df
+def calculate_fold_changes(control_data, treatment_data):
+    return treatment_data.mean(axis=1) / control_data.mean(axis=1)
 
-    def batch_correction(self, df, n_components=2):
-        pca = PCA(n_components=n_components)
-        pca_result = pca.fit_transform(scale(df.T))
-        corrected = df - pca_result.T.dot(pca.components_)
-        return corrected
+def calculate_p_values(control_data, treatment_data):
+    _, p_values = stats.ttest_ind(control_data.T, treatment_data.T, equal_var=False)
+    return p_values
 
-    def calculate(self):
-        df = self.normalize_counts(self.df)
-        df = self.batch_correction(df)
-        self.results = []
-        for condition in self.metadata['condition'].unique():
-            control_df = df.loc[:, self.metadata['condition'] == 'control']
-            treatment_df = df.loc[:, self.metadata['condition'] == condition]
-            results = self.differential_expression(control_df, treatment_df, condition)
-            self.results.append(results)
+def correct_p_values(p_values):
+    _, p_values_corrected, _, _ = smm.multipletests(p_values, method='fdr_bh')
+    return p_values_corrected
 
-    def differential_expression(self, control_df, treatment_df, condition):
-        fold_changes = treatment_df.mean(axis=1) / control_df.mean(axis=1)
-        control_var = control_df.var(axis=1) + 1e-8
-        treatment_var = treatment_df.var(axis=1) + 1e-8
-        t, p_values = stats.ttest_ind_from_stats(
-            control_df.mean(axis=1), np.sqrt(control_var), len(control_df.columns),
-            treatment_df.mean(axis=1), np.sqrt(treatment_var), len(treatment_df.columns),
-            equal_var=False
-        )
-        p_values_corrected = self.adjust_p_values(p_values)
-        results = pd.DataFrame({
-            'gene_id': control_df.index,
+def differential_expression_analysis(countdata, metadata):
+    results = []
+    for condition in metadata['condition'].unique():
+        if condition == "WT":
+            continue
+        control_data = countdata.loc[:, metadata['condition'] == 'WT']
+        treatment_data = countdata.loc[:, metadata['condition'] == condition]
+        fold_changes = calculate_fold_changes(control_data, treatment_data)
+        p_values = calculate_p_values(control_data, treatment_data)
+        p_values_corrected = correct_p_values(p_values)
+        result = pd.DataFrame({
+            'gene_id': countdata.index,
             'condition': condition,
             'fold_change': fold_changes,
             'p_value': p_values,
             'p_value_corrected': p_values_corrected,
         })
-        return results
+        results.append(result)
+    return results
 
-    def adjust_p_values(self, p_values):
-        reject, pvals_corrected, _, _ = smm.multipletests(p_values, method='fdr_bh')
-        return pvals_corrected
-
-    def save_results(self, filename):
-        result_df = pd.concat(self.results)
-        result_df.to_csv(filename, index=False, sep='\t')
+def save_results(results, output_file):
+    result_df = pd.concat(results)
+    result_df.to_csv(output_file, sep='\t', index=False)
 
 def main():
-    parser = argparse.ArgumentParser(description='Calculate differential gene expression.')
-    parser.add_argument('data_file', type=str, help='Path to the input data file.')
-    parser.add_argument('output_file', type=str, help='Path to the output file.')
-    args = parser.parse_args()
-
-    df = pd.read_csv(args.data_file, sep='\t', index_col=0)
-
-    # Generate the metadata based on the column names
-    conditions = df.columns.str.split("_", n=1, expand=True)[0]
-    metadata = pd.DataFrame(conditions, columns=['condition'])
-    metadata.index.name = 'sample_id'
-    metadata.to_csv('metadata.csv', sep='\t')  # Save metadata to file for future reference
-
-    de = DifferentialExpression(args.data_file, 'metadata.csv')
-    de.calculate()
-    de.save_results(args.output_file)
+    countdata = load_data("~/GSE221626_counts.txt")
+    metadata = define_metadata(countdata)
+    countdata = normalize_counts(countdata)
+    countdata = batch_correction(countdata)
+    results = differential_expression_analysis(countdata, metadata)
+    save_results(results, "~/benchmark.txt")
 
 if __name__ == "__main__":
     main()
